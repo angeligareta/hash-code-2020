b5c5f131f13367cc6d166517d69a09ce66cda025
Angel Igareta <angel@igareta.com>
2020-02-21 02:11:56 +0100

    Refactored code

--- src/HashCode.java
+++ src/HashCode.java
@@ -3,32 +3,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-class Global {
-    static int averageFirstComponent = 0;
-    static double averageSecondComponent = 0;
-    static double stdFirstComponent = 0;
-    static double stdSecondComponent = 0;
-}
-
-
-class Book {
-    private final int id;
-    private final int score;
-
-    Book(int id, int score) {
-        this.id = id;
-        this.score = score;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
-
 class Library {
     private final Integer id;
     private ArrayList<Integer> booksIds;
@@ -36,6 +10,7 @@ class Library {
     private final int signupTime;
     private final int booksPerDay;
     private final int totalNumberOfDays;
+    private int scoreSum;
     private boolean seen;
 
     Library(Integer id, String[] books, String[] booksScore, int scanTime, int booksPerDay, int totalNumberOfDays) {
@@ -44,75 +19,55 @@ class Library {
         this.booksPerDay = booksPerDay;
         this.totalNumberOfDays = totalNumberOfDays;
         this.seen = false;
+        this.scoreSum = 0;
 
-        this.booksIds = new ArrayList<Integer>();
-        this.booksIdScore = new HashMap<Integer, Integer>();
-        for (int i = 0; i < books.length; i++) {
-            int bookId = Integer.parseInt(books[i]);
+        this.booksIds = new ArrayList<>();
+        this.booksIdScore = new HashMap<>();
+        for (String book : books) {
+            int bookId = Integer.parseInt(book);
 
             if (!this.booksIds.contains(bookId)) {
                 int bookScore = Integer.parseInt(booksScore[bookId]);
 
                 this.booksIds.add(bookId);
                 this.booksIdScore.put(bookId, bookScore);
+                this.scoreSum += bookScore;
             }
         }
     }
 
-    public void setSeen() {
+    void setSeen() {
         this.seen = true;
     }
 
-    public int getFirstComponent() {
-        int bookScoreSum = 0;
-        for (Integer bookScore : this.booksIdScore.values()) {
-            bookScoreSum += bookScore;
-        }
-        return bookScoreSum;
-    }
-
-    public Double getSecondComponent() {
-        return (double) this.totalNumberOfDays / (double) this.signupTime;
-    }
-
-    public void calculateSum() {
-        Global.averageFirstComponent += getFirstComponent();
-        Global.averageSecondComponent += getSecondComponent();
-    }
-
-    public Double getLibraryScore() {
-//        System.out.println("Book Score Sum");
-//        System.out.println(firstComponent);
-//        System.out.println("Second component");
-//        System.out.println(secondComponent);
-
-//        return 0.5 * ((getFirstComponent() - Global.averageFirstComponent) / Global.stdFirstComponent) +
-//                0.5 * ((getSecondComponent() - Global.averageSecondComponent) / Global.stdSecondComponent); //- Math.pow(2, this.signupTime);
+    Double getLibraryScore() {
         if (seen) {
+            // Put first seen values (omitting them as we are looping)
             return Double.MAX_VALUE;
         } else {
-            return 0.5 * getFirstComponent() + 0.5 * getSecondComponent();
+            return this.scoreSum + Math.pow(this.booksPerDay, 1.3) / Math.pow(this.signupTime, 2);
         }
     }
 
-    public Integer getId() {
+    Integer getId() {
         return id;
     }
 
-    public ArrayList<Integer> getBooksIds() {
+    ArrayList<Integer> getBooksIds() {
         return booksIds;
     }
 
-    public HashMap<Integer, Integer> getBooksScore() {
+    HashMap<Integer, Integer> getBooksScore() {
         return booksIdScore;
     }
 
-    public void removeBooks(List<Integer> booksIdsToRemove) {
+    void removeBooks(List<Integer> booksIdsToRemove) {
         ArrayList<Integer> oldBooksIds = this.getBooksIds();
         for (Integer id : booksIdsToRemove) {
             if (oldBooksIds.contains(id)) {
-                this.booksIds.remove(id);
+                this.scoreSum -= this.booksIdScore.get(id);
                 this.booksIdScore.remove(id);
+                this.booksIds.remove(id);
             }
         }
     }
@@ -120,11 +75,10 @@ class Library {
 
 public class HashCode {
 
-    public static void calculateSolution(String inputFileName, String outputFileName) throws IOException {
+    private static void calculateSolution(String inputFileName, String outputFileName) throws IOException {
         int bookNumber = 0;
         int totalNumberOfDays = 0;
         ArrayList<Library> libraries = new ArrayList<>();
-        int maxNumberOfBooksToTake = 0;
 
         try {
             File dataset = new File(inputFileName);
@@ -133,11 +87,10 @@ public class HashCode {
             if (dataset_reader.hasNextLine()) {
                 String nextLine = dataset_reader.nextLine();
                 String[] description = nextLine.split(" ");
+
                 bookNumber = Integer.parseInt(description[0]);
                 int libraryNumber = Integer.parseInt(description[1]);
                 totalNumberOfDays = Integer.parseInt(description[2]);
-
-                //maxNumberOfBooksToTake = (int) (bookNumber * 0.7);
 
                 nextLine = dataset_reader.nextLine();
                 String[] bookScore = nextLine.split(" ");
@@ -162,19 +115,6 @@ public class HashCode {
         }
 
         FileWriter writer = new FileWriter(outputFileName);
-
-//        libraries.forEach(Library::calculateSum);
-//        int size = libraries.size();
-//        Global.averageFirstComponent = Global.averageFirstComponent / size;
-//        Global.averageSecondComponent = Global.averageSecondComponent / size;
-//        double firstComponentStd = 0;
-//        double secondComponentStd = 0;
-//        for (Library library : libraries) {
-//            firstComponentStd += Math.pow(library.getFirstComponent() - Global.averageFirstComponent, 2);
-//            secondComponentStd += Math.pow(library.getSecondComponent() - Global.averageSecondComponent, 2);
-//        }
-//        Global.stdFirstComponent = Math.sqrt(firstComponentStd / size);
-//        Global.stdSecondComponent = Math.sqrt(secondComponentStd / size);
         libraries.sort(Comparator.comparing(x -> -x.getLibraryScore()));
 
         try {
@@ -194,12 +134,6 @@ public class HashCode {
                     // Sort by books with more score
                     ArrayList<Integer> bookIdsSorted = library.getBooksIds();
                     bookIdsSorted.sort(Comparator.comparing(bookId -> -library.getBooksScore().get(bookId)));
-
-//                    List<Integer> booksToTake = bookIdsSorted.subList(0, bookIdsSorted.size());
-
-//                    if (bookIdsSorted.size() > maxNumberOfBooksToTake) {
-//                        booksToTake = booksToTake.subList(0, maxNumberOfBooksToTake);
-//                    }
 
                     // Show library id and number of books
                     try {
@@ -242,17 +176,58 @@ public class HashCode {
     }
 
     public static void main(String[] args) throws IOException {
-        calculateSolution("./data/a_example.txt", "./data/solution-a.txt");
-        System.out.println("A Done !");
-        calculateSolution("./data/b_read_on.txt", "./data/solution-b.txt");
-        System.out.println("B Done !");
-        calculateSolution("./data/c_incunabula.txt", "./data/solution-c.txt");
-        System.out.println("C Done !");
-        //calculateSolution("./data/d_tough_choices.txt", "./data/solution-d.txt");
-        System.out.println("D Done !");
-        calculateSolution("./data/e_so_many_books.txt", "./data/solution-e.txt");
-        System.out.println("E Done !");
-        calculateSolution("./data/f_libraries_of_the_world.txt", "./data/solution-f.txt");
-        System.out.println("F Done !");
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/a_example.txt", "./data/solution-a.txt");
+                System.out.println("A Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/b_read_on.txt", "./data/solution-b.txt");
+                System.out.println("B Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/c_incunabula.txt", "./data/solution-c.txt");
+                System.out.println("C Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/d_tough_choices.txt", "./data/solution-d.txt");
+                System.out.println("D Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/e_so_many_books.txt", "./data/solution-e.txt");
+                System.out.println("E Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                calculateSolution("./data/f_libraries_of_the_world.txt", "./data/solution-f.txt");
+                System.out.println("F Done !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
